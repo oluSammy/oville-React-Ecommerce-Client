ff72651d2cdfbde92f9c9285388c1654e30f3a4a
oluSammy <olumorinsammy@gmail.com>
2020-07-24 13:53:54 -0700

    restructured market component

--- src/components/Product/Product.component.jsx
+++ src/components/Product/Product.component.jsx
@@ -4,13 +4,23 @@ import productImg from '../../assets/img/Acer.png';
 
 const Product = () => (
     <div className="product">
-        <figure className="product__img-container">
-            <img src={productImg} alt="Acer" className="product__img"/>
-        </figure>
-        <h6 className="product__name">Hp EliteBook</h6>
-        <p className="product__price">&#8358; 790,000</p>
-        <button className="btn-buy">Buy Now</button>
-        <button className="btn-cart">Add To Cart</button>
+        <div className="product__top">
+            <figure className="product__img-container">
+                <img src={productImg} alt="Acer" className="product__img"/>
+            </figure>
+            <h6 className="product__name">Hp EliteBook</h6>
+            <p className="product__price">&#8358; 790,000</p>
+        </div>
+        <div className="product__description">
+            <p className="product__description--text">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cum, odit? 
+                Lorem ipsum dolor sit amet.
+            </p>
+            <div className="product__btn">
+                <button className="btn-buy">Buy Now</button>
+                <button className="btn-cart">Add To Cart</button>
+            </div>
+        </div>
     </div>
 );
 
